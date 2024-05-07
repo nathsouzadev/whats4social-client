125a2d9ac358965d218e04bdf5adfeb35bf74c81
Nathally Souza <nathallyccd@gmail.com>
2024-05-07 18:02:00 -0300

    [chore]set env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        TWITTER_API_KEY: process.TWITTER_API_KEY,
+        REDIRECT_URI: process.REDIRECT_URI,
+        TWITTER_API_SECRET: process.TWITTER_API_SECRET,
+    }
+};
 
 export default nextConfig;
